ce191d479037a49e98be0d75472778219decfdab
glensand <bezborodoff.gleb@gmail.com>
2020-12-22 11:29:58 +0700

    improved git ignore list

--- out/build/x64-Debug (default)/test/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/cpp/weriety/out/build/x64-Debug (default)/test/googletest-src
-# Build directory: C:/cpp/weriety/out/build/x64-Debug (default)/test/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
